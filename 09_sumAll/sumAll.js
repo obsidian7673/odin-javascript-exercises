ef6d33267d476ff61f776316add9e832bcf700e9
Rosemary T <dev7673@proton.me>
2025-07-22 13:01:37 -0500

    Fix sumAll not passing tests

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = (low, high) => {
+  if (
+    (low < 0 || high < 0) ||
+    (!Number.isInteger(low) || !Number.isInteger(high))
+  ) return "ERROR";
 
+  if (low > high) {
+    const temp = low;
+    low = high;
+    high = temp;
+  }
+
+  let sum = 0;
+  for (let i = low; i <= high; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
