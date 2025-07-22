d574d9ebfaa7cb2bef1ec3ef3c9a17841ac7c4dc
Rosemary T <dev7673@proton.me>
2025-07-22 11:13:02 -0500

    Fix repeatString not passing tests

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = (string, numRep) => {
+  if (numRep < 0) { return 'ERROR' }
+  let repeatedString = "";
+  while (numRep) {
+    repeatedString += string;
+    numRep--;
+  }
 
+  return repeatedString;
 };
 
 // Do not edit below this line
