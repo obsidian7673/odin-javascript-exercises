6fda9f15c260635edc3bcca56c7324acc286fca0
Rosemary T <dev7673@proton.me>
2025-07-22 13:40:45 -0500

    Fix tempCenversion not passing tests

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,6 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = (fahrenheit) => Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = (celsius) => Math.round((celsius * 9 / 5 + 32) * 10) / 10;
 
 // Do not edit below this line
 module.exports = {
